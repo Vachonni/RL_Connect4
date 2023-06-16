4cb5d1f17acf3438af92cdb8363eef0d24967d43
Nicholas Vachon MBANV <nicholasvachon@hotmail.com>
2023-06-16 15:56:13 +0000

    Play with seperated agent loading

--- play/KagglePlay.py
+++ play/KagglePlay.py
@@ -4,44 +4,31 @@ import numpy as np
 
 from kaggle_environments import make
 from src.KaggleTest import ConnectFourGym, PPO, policy_kwargs, get_win_percentages
+from src.ModelToAgent import model_to_agent
 
-
-# Choose model
-MODEL_PATH = "../models/first_agent"
+from config import PATH_BASE_MODEL, PATH_ADVERSARY_MODEL, N_ROUNDS
 
 #%%
-# Set environment
-env = ConnectFourGym(agent2="random")
-
-# Load model used by agent
-model = PPO("CnnPolicy", env, policy_kwargs=policy_kwargs, verbose=0)
-model = PPO.load(MODEL_PATH)
-
-# Build agent from model
-def agent(obs, config):
-    # Use the best model to select a column
-    col, _ = model.predict(np.array(obs['board']).reshape(1, 6,7))
-    # Check if selected column is valid
-    is_valid = (obs['board'][int(col)] == 0)
-    # If not valid, select random move. 
-    if is_valid:
-        return int(col)
-    else:
-        return random.choice([col for col in range(config.columns) if obs.board[int(col)] == 0])
-    
+# Load agents
+base_agent = model_to_agent(PATH_BASE_MODEL)
+adv_agent = model_to_agent(PATH_ADVERSARY_MODEL)
 
 #%%
 # Create the game environment
 env = make("connectx")
 
 # Two random agents play one game round
-env.run([agent, "random"])
+env.run([base_agent, adv_agent])
 
 #%%
 # Visual of the game
 env.render(mode="ipython")
 
 #%%
-# Play 100 games
-print(get_win_percentages(agent1=agent, agent2="random", n_rounds=100))
+# Play games
+print(get_win_percentages(agent1=base_agent, 
+                          agent2=adv_agent, 
+                          n_rounds=N_ROUNDS))
 
+
+# %%
