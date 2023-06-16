5e38dba560e00c9026d7dc65ae31135fd78b04ff
Nicholas Vachon MBANV <nicholasvachon@hotmail.com>
2023-06-16 12:03:34 +0200

    First commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages
+from setuptools import setup
+
+setup(
+    name="src",
+    version="0.0.1",
+    maintainer="niv",
+    description=f"Local folders of RL_CONNECT4 as Python module",
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+)
