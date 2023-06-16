0d0b10ca27bb2436375b89363b108fbae1a77977
Nicholas Vachon MBANV <nicholasvachon@hotmail.com>
2023-06-16 15:53:36 +0000

    Basic config file

---
+++ config.py
@@ -0,0 +1,8 @@
+#PATH_BASE_MODEL = "../models/first_agent"
+PATH_BASE_MODEL = "random"
+
+
+# PATH_ADVERSARY_MODEL = "../models/new_agent"
+PATH_ADVERSARY_MODEL = "../models/first_agent"
+
+N_ROUNDS = 1000
