2709813447894dee9ee30fd89810059cf37d710e
Nicholas Vachon MBANV <nicholasvachon@hotmail.com>
2023-06-20 16:12:54 +0000

    LearnToStop new model always start from scratch (random)

--- config.py
+++ config.py
@@ -5,9 +5,9 @@ import os
 #%%
 N_ROUNDS = 100
 
-N_IT = 5000
+N_IT = 1000
 
-PATIENCE = 3
+PATIENCE = 20
 
 
 #%% PATH CONFIG
@@ -19,18 +19,24 @@ path_parent = path_config.split("RL_Connect4")[0]
 path_models = os.path.join(path_parent, "RL_Connect4", "artifacts", "models")
 # Path to learning curves
 path_learning_curves = os.path.join(path_parent, "RL_Connect4", "artifacts", "learning_curves")
+# Path to plots
+path_plots = os.path.join(path_parent, "RL_Connect4", "artifacts", "plots")
 
 
 
 #%% MODELS PATHS
-PATH_BASE_MODEL = "random"
-# PATH_BASE_MODEL = os.path.join(path_models, "third_agent")
+# PATH_BASE_MODEL = "random"
+PATH_BASE_MODEL = os.path.join(path_models, "stop_agent_BR0_1000_100")
 
 PATH_ADVERSARY_MODEL = os.path.join(path_models, "fourth_agent")
 
-PATH_MEW_MODEL = os.path.join(path_models, f"stop_agent_{N_IT}_{N_ROUNDS}")
+PATH_MEW_MODEL = os.path.join(path_models, f"stop_agent_AR1_{N_IT}_{N_ROUNDS}")
 
 
 #%% LEARNING CURVES PATHS
-PATH_ALL_WINS = os.path.join(path_learning_curves, f"all_wins_{N_IT}_{N_ROUNDS}.csv")
+PATH_LEARNING_CURVES = os.path.join(path_learning_curves, f"df_wins_AR1_{N_IT}_{N_ROUNDS}")
+
+
+#%% PLOTS PATHS
+PATH_PLOT = os.path.join(path_plots, f"graph_AR1_{N_IT}_{N_ROUNDS}")
 
--- run/KaggleLearnStop.py
+++ run/KaggleLearnStop.py
@@ -5,39 +5,40 @@ import pandas as pd
 
 from src.KaggleTest import ConnectFourGym, PPO, policy_kwargs, get_win_percentages
 from src.ModelToAgent import modelpath_to_agent, model_to_agent
-from config import N_ROUNDS, N_IT, PATIENCE, PATH_BASE_MODEL, PATH_MEW_MODEL, PATH_ALL_WINS
-
+from config import N_ROUNDS, N_IT, PATIENCE, PATH_BASE_MODEL, PATH_MEW_MODEL, PATH_LEARNING_CURVES, PATH_PLOT
 
 
 def learn_to_stop(path_base_model, path_new_model, n_it=1000, n_rounds=100):
 
     # AGENT SETUP
     # Load adversary agent (the one we want to learn to beat)
-    adv_agent = modelpath_to_agent(path_base_model)
+    base_agent = modelpath_to_agent(path_base_model)
     # Set environment that will reply as adversary agent
-    env_adv = ConnectFourGym(agent2=adv_agent)
+    env_adv = ConnectFourGym(agent2=base_agent)
     model = PPO("CnnPolicy", env_adv, policy_kwargs=policy_kwargs, verbose=0)
 
     # LOOP SETUP
-    all_wins = []
+    all_wins = {"wins_vs_base": [], "wins_vs_random": []}
     new_win = 0.5
-    old_win = 0
+    max_win = 0.5
+    random_win = 0.5
     not_improving = []
     patience = PATIENCE
     stop_criteria = False
 
 
     while stop_criteria is False:
-        print(f"\n\nRound: {len(all_wins)}.")
+        print(f"\n\nRound: {len(all_wins['wins_vs_base'])} -- Score to beat: {max_win}")
         # Save scores
-        all_wins.append([new_win])
+        all_wins["wins_vs_base"].append(new_win)
+        all_wins["wins_vs_random"].append(random_win)
 
         # Update loop variables
-        if new_win > old_win:
+        if new_win > max_win:
             print("New agent is better than old agent, saving new model.")
             # save new model
-            model.save(PATH_MEW_MODEL)
-            old_win = new_win
+            max_win = new_win
+            model.save(path_new_model)
             not_improving = []
         else:
             not_improving.append(new_win)
@@ -56,32 +57,43 @@ def learn_to_stop(path_base_model, path_new_model, n_it=1000, n_rounds=100):
 
         # Get win percentage
         print("Getting win percentage...")
-        _, new_win = get_win_percentages(agent1=adv_agent,
+        _, new_win = get_win_percentages(agent1=base_agent,
                                          agent2=new_agent,
                                          n_rounds=n_rounds)
+        _, random_win = get_win_percentages(agent1="random",
+                                         agent2=new_agent,
+                                         n_rounds=n_rounds)        
 
     # All wins as dataframe
-    all_wins = pd.DataFrame(all_wins, columns=["pourcentage_wins"])
+    df_wins = pd.DataFrame(all_wins)
 
     # Add column with number of iterations
-    all_wins["n_it"] = all_wins.index * n_it
+    df_wins["n_it"] = df_wins.index * n_it
 
-    return all_wins
+    return df_wins, max_win
 
 
 #%%
 if __name__ == "__main__":
-    all_wins = learn_to_stop(path_base_model=PATH_BASE_MODEL,
+    df_wins, max_win = learn_to_stop(path_base_model=PATH_BASE_MODEL,
                              path_new_model=PATH_MEW_MODEL,
                              n_it=N_IT,
                              n_rounds=N_ROUNDS)
 
-    print(all_wins)
+    print(df_wins)
 
-    # Plot all wins with pandas plot
-    all_wins.plot(x="n_it", y="pourcentage_wins")
     # Save all wins
-    all_wins.to_csv(PATH_ALL_WINS, index=False)
+    df_wins.to_csv(PATH_LEARNING_CURVES+"_"+str(int(max_win*100))+".csv", index=False)
+   
+    new_model_name = PATH_MEW_MODEL.split("/")[-1]
+    # Plot all wins with pandas plot
+    fig = df_wins.plot(x="n_it", 
+                       y=["wins_vs_base", "wins_vs_random"], 
+                       title=f"Win ratio {new_model_name}",
+                       ylabel="ratio").get_figure()
+    # Save the plot with panqdas plot
+    fig.savefig(PATH_PLOT+"_"+str(int(max_win*100))+".png")
+
 
 
 
