6feb4156ac376eefbfcab99eae12f483cb84a234
Nicholas Vachon MBANV <nicholasvachon@hotmail.com>
2023-06-20 08:47:12 +0000

    paths working everywhere

--- config.py
+++ config.py
@@ -1,11 +1,22 @@
+
+#%%
+import os
+
+# Get path of current directory
+path_config = os.getcwd()
+# Get path parent RL_Connect4
+path_parent = path_config.split("RL_Connect4")[0]
+# Path to models
+path_models = os.path.join(path_parent, "RL_Connect4", "models")
+
+#%%
 # PATH_BASE_MODEL = "random"
-# PATH_BASE_MODEL = "./models/first_agent"
-# PATH_BASE_MODEL = "./models/second_agent"
-PATH_BASE_MODEL = "./models/third_agent"
+PATH_BASE_MODEL = os.path.join(path_models, "third_agent")
 
-PATH_ADVERSARY_MODEL = "./models/third_agent"
+PATH_ADVERSARY_MODEL = os.path.join(path_models, "fourth_agent")
 
-PATH_MEW_MODEL = "./models/fourth_agent"
+PATH_MEW_MODEL = os.path.join(path_models, "fifth_agent")
 
 
-N_ROUNDS = 1000
+#%%
+N_ROUNDS = 100
