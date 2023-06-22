d106e5a9201ade20c1980ffafa4a00c0a8f49a09
Nicholas Vachon MBANV <nicholasvachon@hotmail.com>
2023-06-22 10:54:14 +0000

    Kernel size 4 (implies padding of 1)

--- src/KaggleTest.py
+++ src/KaggleTest.py
@@ -77,9 +77,9 @@ class CustomCNN(BaseFeaturesExtractor):
         # CxHxW images (channels first)
         n_input_channels = observation_space.shape[0]
         self.cnn = nn.Sequential(
-            nn.Conv2d(n_input_channels, 32, kernel_size=3, stride=1, padding=0),
+            nn.Conv2d(n_input_channels, 32, kernel_size=4, stride=1, padding=1),
             nn.ReLU(),
-            nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=0),
+            nn.Conv2d(32, 64, kernel_size=4, stride=1, padding=1),
             nn.ReLU(),
             nn.Flatten(),
         )
@@ -90,7 +90,11 @@ class CustomCNN(BaseFeaturesExtractor):
                 th.as_tensor(observation_space.sample()[None]).float()
             ).shape[1]
 
-        self.linear = nn.Sequential(nn.Linear(n_flatten, features_dim), nn.ReLU())
+        self.linear = nn.Sequential(
+            nn.Linear(n_flatten, 64), 
+            nn.ReLU(),
+            nn.Linear(64, features_dim), 
+            nn.ReLU())
 
     def forward(self, observations: th.Tensor) -> th.Tensor:
         return self.linear(self.cnn(observations))
@@ -99,7 +103,8 @@ policy_kwargs = dict(
     features_extractor_class=CustomCNN,
 )
         
-
+# ins = CustomCNN(gym.spaces.Box(low=0, high=2, shape=(1,6,7), dtype=int))
+# print(ins) # Check architecture
 
 
     
