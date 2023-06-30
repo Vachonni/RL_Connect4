440f44512a4d496045d6533e335e17a5e9960e85
Nicholas Vachon MBANV <nicholasvachon@hotmail.com>
2023-06-30 13:20:51 +0000

    Registered the env created from Dockerfile

--- train_azure.py
+++ train_azure.py
@@ -3,37 +3,53 @@ import os
 from azureml.core import Workspace, Experiment, Environment, ScriptRunConfig
 from azureml.core.authentication import ServicePrincipalAuthentication
 
-# Get an Azure Service Principal
-svc_pr = ServicePrincipalAuthentication(
-    tenant_id=os.environ['TENANT_ID'],
-    service_principal_id=os.environ['CLIENT_ID'],
-    service_principal_password=os.environ['CLIENT_SECRET'])
 
-# Get an Azure workspace
+# Try Azure Service Principal authentification.
+# If fails, use interactive authentification
+try:
+    svc_pr = ServicePrincipalAuthentication(
+        tenant_id=os.environ['TENANT_ID'],
+        service_principal_id=os.environ['CLIENT_ID'],
+        service_principal_password=os.environ['CLIENT_SECRET'])
+except:
+    svc_pr = None
+
+
+# Get Azure workspace
 ws = Workspace.get(name=os.environ.get('WORKSPACE_NAME'), 
                    subscription_id=os.environ.get('SUBSCRIPTION_ID'), 
                    resource_group=os.environ.get('RESOURCE_GROUP'),
                    auth=svc_pr)
 
-# Set an Azure environment with Dockerfile
-env = Environment.from_dockerfile(name='RLConnect4Package',
-                                  dockerfile='Dockerfile')
 
-# Create Azure ScriptRunConfig
-script_run_config = ScriptRunConfig(source_directory='./run',
-                                    script='KaggleLearnStop.py',
-                                    environment=env)                                    
+# Try to get existing environment. 
+# If fails, create a new one and register it
+try:
+    env = Environment.get(workspace=ws, name='RLConnect4Package')
+except:
+    # Set an Azure environment with Dockerfile
+    env = Environment.from_dockerfile(name='RLConnect4Package',
+                                      dockerfile='Dockerfile')
+    # Register the environment
+    env.register(workspace=ws)
 
-# Create a new experiment
-experiment = Experiment(workspace=ws, name='kaggle-learn-stop')
+    
 
-# Submit the experiment
-run = experiment.submit(config=script_run_config)
+# # Create Azure ScriptRunConfig
+# script_run_config = ScriptRunConfig(source_directory='./run',
+#                                     script='KaggleLearnStop.py',
+#                                     environment=env)                                    
 
-run.log('tryingLOG', "is it working?")
+# # Create a new experiment
+# experiment = Experiment(workspace=ws, name='kaggle-learn-stop')
 
-# Run the experiment
-run.wait_for_completion(show_output=True)
+# # Submit the experiment
+# run = experiment.submit(config=script_run_config)
+
+# run.log('tryingLOG', "is it working?")
+
+# # Run the experiment
+# run.wait_for_completion(show_output=True)
                         
 
 
